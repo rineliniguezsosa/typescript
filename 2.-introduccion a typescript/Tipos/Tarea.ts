169bc337803b22cb09c0b761f000e924523b17da
rinel <rineliniguezsosa@gmail.com>
2023-01-28 15:33:50 -0600

    mis resultados de tipo

--- 2.-introduccion a typescript/Tipos/Tarea.ts
+++ 2.-introduccion a typescript/Tipos/Tarea.ts
@@ -1,36 +1,43 @@
 (() => {
 
     // Tipos
-    const batman = 'Bruce';
-    const superman = 'Clark';
+    const batman:string = 'Bruce';
+    const superman:string = 'Clark';
   
-    const existe = false;
+    const existe:boolean = false;
   
     // Tuplas
-    const parejaHeroes = [batman,superman];
-    const villano = ['Lex Lutor',5,true];
+    const parejaHeroes:[string,string] = [batman,superman];
+    const villano:[string,number,boolean] = ['Lex Lutor',5,true];
   
     // Arreglos
-    const aliados = ['Mujer Maravilla','Acuaman','San', 'Flash'];
+    const aliados:string[] = ['Mujer Maravilla','Acuaman','San', 'Flash'];
   
     //Enumeraciones
     const fuerzaFlash = 5;
     const fuerzaSuperman = 100;
     const fuerzaBatman = 1;
     const fuerzaAcuaman = 0;
+
+    enum superheroes {
+        fuerzaFlash = 5,
+        fuerzaSuperman = 100,
+        fuerzaBatman = 1,
+        fuerzaAcuaman = 0
+    }
   
     // Retorno de funciones
-    function activar_batiseñal(){
+    function activar_batiseñal():string{
       return 'activada';
     }
   
-    function pedir_ayuda(){
+    function pedir_ayuda():void{
       console.log('Auxilio!!!');
     }
   
     // Aserciones de Tipo
     const poder: any = '100';
-    const largoDelPoder:number = poder.length;
+    const largoDelPoder:number = (poder as string).length;
     console.log( largoDelPoder );
   
   
