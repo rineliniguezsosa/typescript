3bc60940e823d905e21176c6501310f07cc8400a
rinel <rineliniguezsosa@gmail.com>
2023-01-28 11:49:01 -0600

    aplicando casting para la variable de tipo string

--- 2.-introduccion a typescript/Tipos/Any.ts
+++ 2.-introduccion a typescript/Tipos/Any.ts
@@ -2,7 +2,7 @@
     // NO UTILIZAR EL TIPO DE DATO ANY CON FRECUENCIA
     let avenger:any = 'Hola mundo!';
 
-    console.log(avenger.charAt(0));
+    console.log((avenger as string).charAt(0));
 
     avenger = 28.8934;
     
