b6ff28c9c76c8a42906818f259d12884e5849840
rinel <rineliniguezsosa@gmail.com>
2023-01-28 12:25:56 -0600

    codigo transpilado del fichero Any.ts

---
+++ 2.-introduccion a typescript/Tipos/Any.js
@@ -0,0 +1,9 @@
+"use strict";
+;
+(function () {
+    // NO UTILIZAR EL TIPO DE DATO ANY CON FRECUENCIA
+    var avenger = 'Hola mundo!';
+    console.log(avenger.charAt(0));
+    avenger = 28.8934;
+    console.log(avenger.toFixed(2));
+})();
