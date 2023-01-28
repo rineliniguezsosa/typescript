5f7ecc155fcb0610381dd1d8e3f9e069b03c2fdb
rinel <rineliniguezsosa@gmail.com>
2023-01-28 02:05:12 -0600

    codigo transpilado del fichero Strings.ts

---
+++ 2.-introduccion a typescript/Tipos/Strings.js
@@ -0,0 +1,8 @@
+"use strict";
+;
+(function () {
+    var batman = 'batman';
+    var linterna = "Linterna verde";
+    var flash = "flash es compa\u00F1ero de " + batman;
+    console.log(flash);
+})();
