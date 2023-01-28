c3854b181c59f799b029bb5655a96d5207cdfbed
rinel <rineliniguezsosa@gmail.com>
2023-01-28 12:49:51 -0600

    transpilando codigo del fichero Arrays.ts

---
+++ 2.-introduccion a typescript/Tipos/Arrays.js
@@ -0,0 +1,7 @@
+"use strict";
+;
+(function () {
+    var numbers = [1, 2, 34, 5];
+    var superheroes = ['flash', 'batman', 'super man', 'linterna verde'];
+    superheroes.forEach(function (v) { return console.log(v.toUpperCase()); });
+})();
