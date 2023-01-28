889531621fbd6c3b2fd972a8b0647de358bcf38a
rinel <rineliniguezsosa@gmail.com>
2023-01-28 14:57:43 -0600

    creando funcion error que retornara un resultado NO exitoso o un numero e imprimira en consola

--- 2.-introduccion a typescript/Tipos/Never.ts
+++ 2.-introduccion a typescript/Tipos/Never.ts
@@ -1,3 +1,12 @@
 (()=>{
+    const error = (msg:string):(never|number) =>{
+        if(false){
+            throw new Error(msg)
+        }
+        return 1;
+    }
 
+    const data = error("error")
+    console.log(data);
+    
 })()
