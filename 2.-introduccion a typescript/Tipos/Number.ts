08a4d4baa6f42e2900dd8c113f4e6b09899ee356
rinel <rineliniguezsosa@gmail.com>
2023-01-28 01:36:44 -0600

    creando ternario para determinar la posición de los avengers y imprimiendo en pantalla

--- 2.-introduccion a typescript/Tipos/Number.ts
+++ 2.-introduccion a typescript/Tipos/Number.ts
@@ -1,4 +1,9 @@
 ;(() => {
     const avenger:number = 10;
     const villan:number = 20;
+
+    const result:string = ( avenger < villan ) ? 'estamos en problemas' : 'nos salvamos'
+
+    console.log(result);
+    
 })()
