e1cd96741ef2aeeba4f0b0549bfcbac9c255ff30
rinel <rineliniguezsosa@gmail.com>
2023-01-28 01:38:13 -0600

    transpilando el contenido del fichero Number.ts

---
+++ 2.-introduccion a typescript/Tipos/Number.js
@@ -0,0 +1,8 @@
+"use strict";
+;
+(function () {
+    var avenger = 10;
+    var villan = 20;
+    var result = (avenger < villan) ? 'estamos en problemas' : 'nos salvamos';
+    console.log(result);
+})();
