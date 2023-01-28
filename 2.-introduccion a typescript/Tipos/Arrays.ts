9e6071553791393920781cc1f465c2036d56eab7
rinel <rineliniguezsosa@gmail.com>
2023-01-28 12:49:02 -0600

    creando array de tipo string y recorriendo cada uno de sus valores a mayusculas

--- 2.-introduccion a typescript/Tipos/Arrays.ts
+++ 2.-introduccion a typescript/Tipos/Arrays.ts
@@ -1,4 +1,7 @@
 ;(() => {
     const numbers: number[] = [1,2,34,5];
     
+    const superheroes:string[] = ['flash','batman','super man','linterna verde'];
+
+    superheroes.forEach( v => console.log(v.toUpperCase()))
 })()
