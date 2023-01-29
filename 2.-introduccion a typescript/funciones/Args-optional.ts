8b983c561a664738e4381ebf064afdc509bea39d
rinel <rineliniguezsosa@gmail.com>
2023-01-28 19:20:27 -0600

    estableciendo parametro opcional a la función fullname

--- 2.-introduccion a typescript/funciones/Args-optional.ts
+++ 2.-introduccion a typescript/funciones/Args-optional.ts
@@ -1,3 +1,9 @@
 (()=>{
-    
+    const fullname = (name: string,firstname:string,lastname?:string):string =>{
+        return `Su nombre completo es: ${name} ${firstname} ${lastname || ''}`
+    }
+
+    const data = fullname('rinel','rinel')
+
+    console.log(data);
 })()
