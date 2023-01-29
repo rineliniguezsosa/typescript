89b5d8eb1ba45a7d18504c860c5d38de8100fc74
rinel <rineliniguezsosa@gmail.com>
2023-01-28 20:15:17 -0600

    parametro rest en la función fullname

--- 2.-introduccion a typescript/funciones/Args-rest.ts
+++ 2.-introduccion a typescript/funciones/Args-rest.ts
@@ -1,3 +1,9 @@
 ;(() => {
-    
+    const fullname = (name: string,...fullname:string[]):string =>{
+        return `Su nombre completo es: ${name} ${fullname.join(' ')}`
+    }
+
+    const data = fullname('rinel','rinel','sosa')
+
+    console.log(data);
 })()
