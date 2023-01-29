c60dd172cda092aea8ea3ecf5be25c479e2bd245
rinel <rineliniguezsosa@gmail.com>
2023-01-28 21:25:13 -0600

    tipo Function

--- 2.-introduccion a typescript/funciones/Type-funcion.ts
+++ 2.-introduccion a typescript/funciones/Type-funcion.ts
@@ -1,3 +1,18 @@
 ;(() => {
+    const greet = ( name:string ):string => `Hola ${name}`;
+    const save = ():string => 'save the world'
+    const suma = (num:number,num2:number):number=> num + num2;
+
+    let mifuncion:Function; //pasar referencias
+
+    mifuncion = greet 
+    console.log(mifuncion('rinel'));
+
+    mifuncion = save
+    console.log(mifuncion())
+
+    mifuncion = suma
+    console.log(mifuncion(10,2));
+    
     
 })()
