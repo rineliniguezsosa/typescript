15a8e4e74e6f858fd4da6b322a02037def516b5a
rinel <rineliniguezsosa@gmail.com>
2023-01-30 12:15:55 -0600

    llamando a super para poder heredar las propiedades name y power además creamos el objeto flyavenger

--- 2.-introduccion a typescript/ES6/clasesES6.js
+++ 2.-introduccion a typescript/ES6/clasesES6.js
@@ -7,13 +7,14 @@
     }
 
     class Flyavenger extends Avenger{
-        constructor(){
+        constructor(name,power){
+            super(name,power)
             this.vuela = true;
         }
     }
 
     const avenger = new Avenger('iron man','super machine')
-    const flyavenger = new Flyavenger()
+    const flyavenger = new Flyavenger('falcon','cuerpo a cuerpo')
 
     console.log(flyavenger);
 })()
