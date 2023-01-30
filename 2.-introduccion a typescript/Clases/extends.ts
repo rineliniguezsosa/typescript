f1fb390086320837b9ebaa87cdea51f9df88396a
rinel <rineliniguezsosa@gmail.com>
2023-01-30 15:03:52 -0600

    defniendo el metodo getfullname para devolver el nombre completo del objeto

--- 2.-introduccion a typescript/Clases/extends.ts
+++ 2.-introduccion a typescript/Clases/extends.ts
@@ -1,7 +1,11 @@
 ;(() => {
     class Avenger{
         constructor(public name:string,public realname:string){
+            
+        }
 
+        private getfullname(){
+            return `${this.name}  ${this.realname}`
         }
     }
 })()
