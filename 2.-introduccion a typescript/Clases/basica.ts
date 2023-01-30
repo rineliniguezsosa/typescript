f3766e1e2099e159ea9adf5d9e1495f0ff12ee5b
rinel <rineliniguezsosa@gmail.com>
2023-01-30 14:53:53 -0600

    metodo publico y statico con sus respectivas llamadas

--- 2.-introduccion a typescript/Clases/basica.ts
+++ 2.-introduccion a typescript/Clases/basica.ts
@@ -9,9 +9,21 @@
             this.team = team;
             this.realname = realname;
         }
+
+        public bio(){
+            return `${this.name}`
+        }
+
+        static getavgage(){
+            return this.name
+        }
     }
 
     const antman:Avenger = new Avenger('ant-man','cap america')
+
+    // console.log(antman.bio()) imposible de acceder metodo private
+
+    // console.log(Avenger.getavgage()) metodo static
     console.log(antman);
     
 })()
