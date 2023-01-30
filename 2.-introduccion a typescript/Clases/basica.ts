8f1ace7e48e59dec89f2ded748d80e65b0c02c15
rinel <rineliniguezsosa@gmail.com>
2023-01-30 12:45:12 -0600

    inicializando las variables name,team,realname en el constructor

--- 2.-introduccion a typescript/Clases/basica.ts
+++ 2.-introduccion a typescript/Clases/basica.ts
@@ -3,8 +3,10 @@
         private name:string;
         public team:string;
         public realname:string;
-        constructor(){
-            
+        constructor(name:string,team:string,realname:string){
+            this.name = name;
+            this.team = team;
+            this.realname = realname;
         }
     }
 })()
