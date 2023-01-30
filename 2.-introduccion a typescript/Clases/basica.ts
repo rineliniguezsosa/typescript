14dfeb3fd2313bba97c7c1b9cfaa147aa131b830
rinel <rineliniguezsosa@gmail.com>
2023-01-30 14:28:58 -0600

    propiedades en una sola linea

--- 2.-introduccion a typescript/Clases/basica.ts
+++ 2.-introduccion a typescript/Clases/basica.ts
@@ -1,10 +1,10 @@
 ;(() => {
     class Avenger{
-        private name:string;
-        private team:string;
-        public realname?:string;
+        // private name:string;
+        // private team:string;
+        // public realname?:string;
         static avgname:number = 35;
-        constructor(name:string,team:string,realname?:string){
+        constructor(private name:string,private team:string,public realname?:string){
             this.name = name;
             this.team = team;
             this.realname = realname;
