d7f865d11348b6fb0a0bcda30f05a8267911136c
rinel <rineliniguezsosa@gmail.com>
2023-01-30 12:51:41 -0600

    estableciendo la variable realname como opcional

--- 2.-introduccion a typescript/Clases/basica.ts
+++ 2.-introduccion a typescript/Clases/basica.ts
@@ -2,9 +2,9 @@
     class Avenger{
         private name:string;
         private team:string;
-        public realname:string;
+        public realname?:string;
         static avgname:number = 35;
-        constructor(name:string,team:string,realname:string){
+        constructor(name:string,team:string,realname?:string){
             this.name = name;
             this.team = team;
             this.realname = realname;
