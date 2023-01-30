edcacb336ab8718ded61bc0d4f1f55cbb8e336d2
rinel <rineliniguezsosa@gmail.com>
2023-01-30 10:28:04 -0600

    let y const variables

--- 2.-introduccion a typescript/ES6/let-var.ts
+++ 2.-introduccion a typescript/ES6/let-var.ts
@@ -1,3 +1,11 @@
 ;(() => {
-    
+
+    let data:string = "rinel";
+    const info = {
+        nombre:'rinel',
+        mail:'rineliniguezsosa@gmail.com'
+    }
+
+    info.nombre = "rolando"; // no aceptable para un objeto de tipo constante
+    console.log(data);
 })()
