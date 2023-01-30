97d8803de375d1ddf3bbff4a3609f27c9d880152
rinel <rineliniguezsosa@gmail.com>
2023-01-30 10:28:46 -0600

    transpilado de codigo let-var.ts

---
+++ 2.-introduccion a typescript/ES6/let-var.js
@@ -0,0 +1,11 @@
+"use strict";
+;
+(() => {
+    let data = "rinel";
+    const info = {
+        nombre: 'rinel',
+        mail: 'rineliniguezsosa@gmail.com'
+    };
+    info.nombre = "rolando"; // no aceptable para un objeto de tipo constante
+    console.log(data);
+})();
