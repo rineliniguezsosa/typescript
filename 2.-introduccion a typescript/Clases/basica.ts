6bc832c6c7466ef9329469fc83981681d89588ca
rinel <rineliniguezsosa@gmail.com>
2023-01-30 12:47:48 -0600

    definiendo variable static avgname y estableciendole valor por defecto

--- 2.-introduccion a typescript/Clases/basica.ts
+++ 2.-introduccion a typescript/Clases/basica.ts
@@ -3,6 +3,7 @@
         private name:string;
         private team:string;
         public realname:string;
+        static avgname:number = 35;
         constructor(name:string,team:string,realname:string){
             this.name = name;
             this.team = team;
