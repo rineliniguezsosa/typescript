39c49745b9ecd6e35eb48719512cf2a57dabb00b
rinel <rineliniguezsosa@gmail.com>
2023-01-31 17:45:13 -0600

    codigo transpilado del fichero clases.ts

---
+++ 2.-introduccion a typescript/Namespaces/clases.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Hero = void 0;
+//EXPORT
+class Hero {
+    constructor(name, edad, powerid) {
+        this.name = name;
+        this.edad = edad;
+        this.powerid = powerid;
+    }
+}
+exports.Hero = Hero;
