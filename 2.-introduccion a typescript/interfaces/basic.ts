2bd667b3fd1bbaa55a1cb039aa1c671441eb53c4
rinel <rineliniguezsosa@gmail.com>
2023-01-30 20:41:31 -0600

    interfaz basica

--- 2.-introduccion a typescript/interfaces/basic.ts
+++ 2.-introduccion a typescript/interfaces/basic.ts
@@ -1,3 +1,25 @@
 (()=>{
+    interface heroes  {
+        nombre:string,
+        edad:number,
+        poderes:string[],
+        getnombre?:()=> string //TIPO METODO
+    }
 
+    let flash: heroes = {
+        nombre:'barry allen',
+        edad:25,
+        poderes:['super velocidad','viajar en el tiempo']
+    }
+
+    let superman:heroes = {
+        nombre:'clark kent',
+        edad:22,
+        poderes:['super fuerza','volar','super velocidad','vision rayos x'],
+        getnombre(){
+            return this.nombre;
+        }
+    }
+
+    console.log(superman.nombre);
 })()
