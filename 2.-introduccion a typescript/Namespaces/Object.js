ad9ff54749acac525ccd9d0c07fcecb3be842660
rinel <rineliniguezsosa@gmail.com>
2023-01-31 17:46:01 -0600

    codigo transpilado del fichero Object.ts

---
+++ 2.-introduccion a typescript/Namespaces/Object.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const clases_1 = require("./clases");
+const nuevo = new clases_1.Hero('rinel', 26, 'programming');
+console.log(nuevo);
