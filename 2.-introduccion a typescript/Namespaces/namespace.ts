bffa8307bd7d43d97c18dbf893388a359facf2e7
rinel <rineliniguezsosa@gmail.com>
2023-01-31 16:02:05 -0600

    creando funcion validateDate para la validacion de la cadena de tipo Date

--- 2.-introduccion a typescript/Namespaces/namespace.ts
+++ 2.-introduccion a typescript/Namespaces/namespace.ts
@@ -4,4 +4,8 @@ namespace validatetext {
     const validatetext = (text:string):boolean => {
         return text.length > 3 ? true : false;
     }
+
+    const validateDate = (myDate:Date):boolean => {
+        return isNaN(myDate.valueOf()) ? false : true;
+    }
 }
