6e23b0551bbcead27f2027651dfdc9c96653ce1e
rinel <rineliniguezsosa@gmail.com>
2023-01-30 19:56:05 -0600

    modificando la creacion del objeto Xmen

--- 2.-introduccion a typescript/Clases/Abstract.ts
+++ 2.-introduccion a typescript/Clases/Abstract.ts
@@ -6,8 +6,8 @@
     }
 
     class Xmen extends Mutante{}
-    
-    const wolverin:Mutante = new Mutante('wolverin','logan') 
+
+    const wolverin:Mutante = new Xmen('wolverin','logan') 
     console.log(wolverin);
     
 })()
