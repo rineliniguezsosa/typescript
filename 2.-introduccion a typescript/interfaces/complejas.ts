f23dba3b7ec32f9f3a1b1584107fdd7b59733942
rinel <rineliniguezsosa@gmail.com>
2023-01-30 20:55:52 -0600

    creando el objeto client

--- 2.-introduccion a typescript/interfaces/complejas.ts
+++ 2.-introduccion a typescript/interfaces/complejas.ts
@@ -8,4 +8,14 @@
         nombre:'rinel',
         edad:27
     }
+
+    const client:Cliente ={
+        nombre:'fernando',
+        edad:24,
+        adress:{
+            id:234,
+            zip:'KJY YHB',
+            city:'chenku'
+        }
+    }
 })()
