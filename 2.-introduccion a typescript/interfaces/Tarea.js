7fb6401a5179a3735c02f852be34599b1ebb694a
rinel <rineliniguezsosa@gmail.com>
2023-01-31 14:29:40 -0600

    codigo transpilado del fichero Tarea.ts

---
+++ 2.-introduccion a typescript/interfaces/Tarea.js
@@ -0,0 +1,38 @@
+"use strict";
+// Crear interfaces
+const conducirBatimovil = (auto) => {
+    auto.encender = true;
+    auto.velocidadMaxima = 100;
+    auto.acelear();
+};
+const batimovil = {
+    encender: false,
+    velocidadMaxima: 0,
+    acelear() {
+        console.log("...... gogogo!!!");
+    }
+};
+const guason = {
+    reir: true,
+    comer: true,
+    llorar: false
+};
+const reir = (guason) => {
+    if (guason.reir) {
+        console.log("JAJAJAJA");
+    }
+};
+const ciudadGotica = (ciudadanos) => {
+    return ciudadanos.length;
+};
+class Persona {
+    constructor(nombre, edad, sexo, estadoCivil) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.sexo = sexo;
+        this.estadoCivil = estadoCivil;
+    }
+    imprimirBIO() {
+        console.log("me ejecute en consola  rinel");
+    }
+}
