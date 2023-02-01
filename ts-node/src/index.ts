df26190f017ee766792d221db55e061ec671929e
rinel <rineliniguezsosa@gmail.com>
2023-02-01 17:47:23 -0600

    añadiendo el argumento a la funcion getpokemon ademas de añadir el .then si soluciono la respuesta .catch si la respuesta falla y finally como informacion al termino de la consulta

--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -1,3 +1,7 @@
 import { getPokemon } from './generics/getpokemon';
 
-getPokemon()
+getPokemon(1)
+    .then( resp => console.log(resp))
+    .catch( error => console.log(error))
+    .finally( ()=> console.log('termino de consulta de la pokeapi'))
+    
