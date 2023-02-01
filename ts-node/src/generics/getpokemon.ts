c7ff904e3675c9983c35fdbd3b41d352d6c3a7dc
rinel <rineliniguezsosa@gmail.com>
2023-02-01 17:44:13 -0600

    eliminando el console.log

--- ts-node/src/generics/getpokemon.ts
+++ ts-node/src/generics/getpokemon.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const getPokemon = async (pokemon:string | number) =>{
-    console.log();
+
     try{
         const request = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
         console.log(request);
