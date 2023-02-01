e2655f01f1f7b9bccbd45886301eca4e25c354de
rinel <rineliniguezsosa@gmail.com>
2023-02-01 17:30:14 -0600

    imprimimos en consola el valor de la petecion

--- ts-node/src/generics/getpokemon.ts
+++ ts-node/src/generics/getpokemon.ts
@@ -3,5 +3,6 @@ import axios from 'axios'
 export const getPokemon = async (pokemon:string | number) =>{
     console.log();
     const request = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+    console.log(request);
     
 }
