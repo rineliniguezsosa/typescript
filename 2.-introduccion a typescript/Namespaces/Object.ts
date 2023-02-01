a0d674fdc6930461ad9411eb56fdc3eb38c3a128
rinel <rineliniguezsosa@gmail.com>
2023-01-31 18:08:01 -0600

    importando data e imprimiendo en consola

--- 2.-introduccion a typescript/Namespaces/Object.ts
+++ 2.-introduccion a typescript/Namespaces/Object.ts
@@ -1,6 +1,9 @@
 //IMPORT
 //import { Hero as superheroe, Hero } from './clases';
 import * as superheroe from './clases'; //* importacion completa
+import data from './data/powers';
+
+console.log(data);
 
 //const nuevo = new superheroe('rinel',26,'programming')
 const nuevo2 = new superheroe.Hero('rinel',26,'programming')
