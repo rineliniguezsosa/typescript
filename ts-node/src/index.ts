cb34671fcf4831a8562538da5b2c498b06bacbef
rinel <rineliniguezsosa@gmail.com>
2023-02-01 15:00:51 -0600

    imprimiendo en consola las funciones

--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -1,8 +1,16 @@
 import { Objectgeneric,genericFunction } from "./generics/generic";
 
-Objectgeneric("12233")
-Objectgeneric(123)
-Objectgeneric([1,2,3,true])
+console.log(Objectgeneric("12233").charAt(2));
+console.log(Objectgeneric(123));
+console.log(Objectgeneric([1,2,3,true]));
+
+
+console.log(genericFunction(123).toFixed(1));
+console.log(genericFunction(new Date()).getMonth());
+console.log(genericFunction('Hola mundo rinel').toUpperCase());
+
+
+
 
 
 
