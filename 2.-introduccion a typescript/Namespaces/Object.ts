e6b2ccaab0063364309ed72ad235788c259a8838
rinel <rineliniguezsosa@gmail.com>
2023-01-31 18:01:12 -0600

    comentando las lineas y agregando la importacion completa del fichero clases.ts mediante el simbolo  (*)

--- 2.-introduccion a typescript/Namespaces/Object.ts
+++ 2.-introduccion a typescript/Namespaces/Object.ts
@@ -1,7 +1,8 @@
 //IMPORT
-import { Hero as superheroe } from "./clases";
+//import { Hero as superheroe, Hero } from './clases';
+import * as superheroe from './clases'; //* importacion completa
 
-const nuevo = new superheroe('rinel',26,'programming')
-
-console.log(nuevo);
+//const nuevo = new superheroe('rinel',26,'programming')
+const nuevo2 = new superheroe.Hero('rinel',26,'programming')
+console.log(nuevo2);
 
