dc5116054e222576492a8b5dc30b70a9f852ee2b
rinel <rineliniguezsosa@gmail.com>
2023-02-01 16:56:41 -0600

    cambiando los paths y guardando el contendido del fichero index.ts

---
+++ ts-node/src/backs/generics.ts
@@ -0,0 +1,30 @@
+import { Objectgeneric,genericFunction } from "../generics/generic";
+// import { villan } from './interfaces/Villan';
+// import { hero } from './interfaces/Hero'
+import { villan,hero } from '../interfaces';
+
+console.log(Objectgeneric("12233").charAt(2));
+console.log(Objectgeneric(123));
+console.log(Objectgeneric([1,2,3,true]));
+
+
+console.log(genericFunction(123).toFixed(1));
+console.log(genericFunction(new Date()).getMonth());
+console.log(genericFunction('Hola mundo rinel').toUpperCase());
+
+//CRERANDO EL OBJETO DEADPOOL
+const deadpool = {
+    name:'dead pool',
+    realname:'wade winston wilson',
+    dangerlevel:150
+}
+
+console.log(genericFunction<villan>(deadpool).dangerlevel)
+
+
+
+
+
+
+
+
