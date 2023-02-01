73f5e71a49c0dead1d224f981aa154983dc61d90
rinel <rineliniguezsosa@gmail.com>
2023-02-01 17:42:46 -0600

    añadiendo un try catch

--- ts-node/src/generics/getpokemon.ts
+++ ts-node/src/generics/getpokemon.ts
@@ -2,7 +2,13 @@ import axios from 'axios'
 
 export const getPokemon = async (pokemon:string | number) =>{
     console.log();
-    const request = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
-    console.log(request);
+    try{
+        const request = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+        console.log(request);
+    }
+    catch(error){
+        console.log('algo salio mal :(',error);
+    }
+    
     
 }
