19a6abdb0de16a000b32899e95eb8f8b471ea337
rinel <rineliniguezsosa@gmail.com>
2023-02-02 11:49:59 -0600

    aplicando validacion y extrayendo el argumento mediante el descriptor.value

--- ts-node/src/decoradores/class-pokemon.ts
+++ ts-node/src/decoradores/class-pokemon.ts
@@ -1,6 +1,14 @@
 function checkpokemonId(){
     return function(target:any,propertyKey:string,descriptor:PropertyDescriptor){
         const originalMethod = descriptor.value
+        descriptor.value = (id:number) =>{
+            if(id < 1 || id > 800){ 
+                return console.error(`el pokemon debe de estar entre 1 y 800 `);
+            }else{
+                return originalMethod(id)
+            }
+
+        }
     }
 }
 
