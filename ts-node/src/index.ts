1049d516b90a98be7b67d3a57c2b4070dd2f6df7
rinel <rineliniguezsosa@gmail.com>
2023-02-01 19:04:26 -0600

    obteniendo el pokemon.name de el pokemon enviado por argumento

--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -1,7 +1,7 @@
 import { getPokemon } from './generics/getpokemon';
 
 getPokemon(1)
-    .then( resp => console.log(resp))
+    .then( pokemon => console.log(pokemon.name))
     .catch( error => console.log(error))
     .finally( ()=> console.log('termino de consulta de la pokeapi'))
     
