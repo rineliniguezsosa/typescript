f74b37586f875b82efabc5c495d00b9258e952d9
rinel <rineliniguezsosa@gmail.com>
2023-02-02 12:43:01 -0600

    escuchando los cambios en el puerto 3000 y desplegando el servidor mediante app.listen

--- express-api/index.js
+++ express-api/index.js
@@ -2,3 +2,7 @@ const express = require('express')
 const app = express()
 
 const PORT = 3000;
+
+app.listen(()=>{
+    console.log(`Èl servidor esta escuchando los cambios en http://localhost:${PORT}`)
+})
